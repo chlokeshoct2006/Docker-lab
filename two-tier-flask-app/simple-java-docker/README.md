3f23d7538fe78b9abfe76e61504c5f24867058ed
lokesh chaudhary <chlokeshoct2006@gmail.com>
2025-08-31 21:01:01 +0530

    Properly added two-tier-flask-app contents

---
+++ two-tier-flask-app/simple-java-docker/README.md
@@ -0,0 +1,14 @@
+# Simple Java Docker Project
+
+This is a simple Java application that runs inside a Docker container and prints a custom message along with the 
+current date.
+
+## 📂 Project Structure
+
+ simple-java-docker/
+
+├── Dockerfile<br>
+├── src/<br>
+│ └── Main.java<br>
+└── README.md
+
---
+++ two-tier-flask-app/simple-java-docker/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+	System.out.println("Hello Dosto! Ye mera docker ka phla task h, to print current date!!!");
+        System.out.println("Hello, Docker! Current date: " + currentDate);
+    }
+}
